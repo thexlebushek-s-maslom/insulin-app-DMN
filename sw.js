61560fb7791b7853e5738b54071d43f51b2f5d35
thexlebushek-s-maslom <thexlebushek@gmail.com>
2026-03-23 21:05:04 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,29 +1,44 @@
-const CACHE = 'insulin-v1';
+const CACHE = 'insulin-v2';
 const ASSETS = [
   '/',
   '/index.html',
+  '/about.html',
+  '/privacy.html',
+  '/style.css',
   '/manifest.json',
   '/icons/icon-192.png',
   '/icons/icon-512.png',
-  'https://fonts.googleapis.com/css2?family=DM+Mono:wght@300;400;500&family=DM+Sans:wght@300;400;500;600&display=swap'
 ];
 
 self.addEventListener('install', e => {
   e.waitUntil(
-    caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting())
+    caches.open(CACHE)
+      .then(c => c.addAll(ASSETS))
+      .then(() => self.skipWaiting())
   );
 });
 
 self.addEventListener('activate', e => {
   e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-    ).then(() => self.clients.claim())
+    caches.keys()
+      .then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
   );
 });
 
 self.addEventListener('fetch', e => {
   e.respondWith(
-    caches.match(e.request).then(cached => cached || fetch(e.request).catch(() => caches.match('/index.html')))
+    caches.match(e.request)
+      .then(cached => cached || fetch(e.request)
+        .then(res => {
+          // Cache successful GET responses
+          if (e.request.method === 'GET' && res.status === 200) {
+            const copy = res.clone();
+            caches.open(CACHE).then(c => c.put(e.request, copy));
+          }
+          return res;
+        })
+        .catch(() => caches.match('/index.html'))
+      )
   );
 });
